de7a836903d043f6335de0802da4a218c2ec53e4
Ben Orenstein <ben.orenstein@gmail.com>
2016-10-30 00:32:08 -0700

    Generate 50 randos

--- src/barb/core.cljs
+++ src/barb/core.cljs
@@ -1,23 +1,17 @@
 (ns barb.core
-  (:require [clojure.browser.repl :as repl]))
+  (:require [clojure.browser.repl :as repl]
+            [cljs.spec :as s]))
 
-;; (defonce conn
-;;   (repl/connect "http://localhost:9000/repl"))
+(defonce conn
+  (repl/connect "http://localhost:9000/repl"))
 
 (def image-size 100)
+(def population-count 50)
 
 (enable-console-print!)
 
-;; (defn make-context []
-;;   "Creates the native js context object"
-;;   (let [canvas (.getElementById js/document "#canvas")
-;;         context (.getContext canvas "2d")]
-;;     (set! (.-width canvas) 100)
-;;     (set! (.-height canvas) 100)
-;;     (set! (.-fillStyle context) "rgb(0, 0, 0)")
-;;     (set! (.-lineWidth context) wall-width-in-pixels)
-;;     context))
-;;
+(defn log [x]
+  (.log js/console x))
 
 (defn read-reference-image-as-rgb []
   (let [canvas (.getElementById js/document "reference-canvas")
@@ -26,12 +20,32 @@
     (set! (.-width canvas) image-size)
     (set! (.-height canvas) image-size)
     (.drawImage context image 0 0)
-    (.getImageData context 0 0 image-size image-size)))
+    (.-data 
+      (.getImageData context 0 0 image-size image-size))))
+
+(defn generate-random-individual []
+  ;;   individuals are maps
+  ;;   x1 y1 x2 y2 x3 y3 r g b a
+  ;;   [0 0 0 0 0 0 128 128 128 128]
+  ;;  TODO: spec for this
+  { 
+   :x1 (rand-int 256)
+   :y1 (rand-int 256)
+   :x2 (rand-int 256)
+   :y2 (rand-int 256)
+   :x3 (rand-int 256)
+   :y3 (rand-int 256)
+   :r (rand-int 256)
+   :g (rand-int 256)
+   :b (rand-int 256)
+   :a (rand-int 256) 
+   })
 
 (defn run []
   (println "Running")
   (let [image-data (read-reference-image-as-rgb)]
-    (.log js/console image-data)))
+    (println (doall (repeatedly population-count generate-random-individual)))
+    (println "Done")))
 
 (.addEventListener
   js/window
