99818be8bff93879f5b09aa1f1190e3eb06ac1cd
Ben Orenstein <ben.orenstein@gmail.com>
2016-10-30 08:50:28 -0700

    more progress

--- src/barb/core.cljs
+++ src/barb/core.cljs
@@ -7,44 +7,50 @@
 
 (def image-size 100)
 (def population-count 50)
+(def polygon-count 12)
 
 (enable-console-print!)
 
 (defn log [x]
   (.log js/console x))
 
-(defn read-reference-image-as-rgb []
+(defn reference-image->image-data []
   (let [canvas (.getElementById js/document "reference-canvas")
         image (.getElementById js/document "reference")
         context (.getContext canvas "2d")]
     (set! (.-width canvas) image-size)
     (set! (.-height canvas) image-size)
     (.drawImage context image 0 0)
-    (.-data 
-      (.getImageData context 0 0 image-size image-size))))
+    (.from js/Array
+      (.-data
+        (.getImageData context 0 0 image-size image-size)))))
 
-(defn generate-random-individual []
-  ;;   individuals are maps
-  ;;   x1 y1 x2 y2 x3 y3 r g b a
-  ;;   [0 0 0 0 0 0 128 128 128 128]
+(defn generate-random-polygon []
   ;;  TODO: spec for this
   { 
-   :x1 (rand-int 256)
-   :y1 (rand-int 256)
-   :x2 (rand-int 256)
-   :y2 (rand-int 256)
-   :x3 (rand-int 256)
-   :y3 (rand-int 256)
-   :r (rand-int 256)
-   :g (rand-int 256)
-   :b (rand-int 256)
-   :a (rand-int 256) 
+   :x1 (rand-int 256) :y1 (rand-int 256)
+   :x2 (rand-int 256) :y2 (rand-int 256)
+   :x3 (rand-int 256) :y3 (rand-int 256)
+   :r (rand-int 256) :g (rand-int 256) :b (rand-int 256) :a (rand-int 256) 
    })
+
+(defn generate-random-individual []
+  "An individual is a collection of polygons"
+  (repeatedly polygon-count generate-random-polygon))
+
+(defn individual->image-data
+  "Take a vector of maps representing an individual and return a vector of rgba
+  data."
+  [individual]
+  ;; make a canvas -> context
+  ;; draw each polygon on context (lines and colors and alpha)
+  nil
+  )
+
 
 (defn run []
   (println "Running")
-  (let [image-data (read-reference-image-as-rgb)]
-    (println (doall (repeatedly population-count generate-random-individual)))
+  (let [image-data (reference-image->image-data)]
     (println "Done")))
 
 (.addEventListener
